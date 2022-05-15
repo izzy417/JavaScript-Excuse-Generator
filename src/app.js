2c2da4077bb2c3aefa2659c8bdbc0ed3741f79f9
izzy417 <israeldail2@gmail.com>
2022-05-15 02:00:58 +0000

    project done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = genericExcuse();
+  console.log("hello");
+};
+
+let genericExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * 4);
+  let actionIndex = Math.floor(Math.random() * 4);
+  let whatIndex = Math.floor(Math.random() * 3);
+  let whenIndex = Math.floor(Math.random() * 5);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
